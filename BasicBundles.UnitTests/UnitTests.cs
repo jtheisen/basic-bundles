19e25545a33ebee3a78aa1d6dd9a0f45870448bc
jens <jens.theisen@monkeybusters.de>
2016-06-18 00:17:29 +0200

    Unit test project.

---
+++ BasicBundles.UnitTests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronStone.Web.BasicBundles.Testing
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void RunAllTests()
+        {
+            Tests.RunTests();
+        }
+    }
+}

c446ed31ff52db7dcac9a9bf67ecd57716d0c5bd
jens <jens.theisen@monkeybusters.de>
2016-07-02 00:10:32 +0200

    version update script

---
+++ update-version.ps1
@@ -0,0 +1,36 @@
+param (
+    [switch] $includeParticulars = $false
+)
+
+$path = "C:\Users\Jens\Dropbox (VisBricks)\Projects\ironstone-basic-bundles\BasicBundles";
+
+$version = [System.IO.File]::ReadAllText("$path\version.txt")
+
+$versionUntilDash = $version.Split('-')[0];
+
+if ($includeParticulars) {
+    $date = [DateTime]::Now.ToString("yyMMdd-HHmm");
+
+    $ref = $(git -C $path rev-parse --short HEAD)
+
+    $longversion = "${version}:${ref}:$date"
+}
+else {
+    $longversion = $version
+}
+
+foreach ($file in dir -Recurse $path | where { $_.Name -eq "AssemblyInfo.cs" }) {
+    (gc $file.FullName) `
+    | foreach { $_ -replace '\[assembly: AssemblyVersion\(\"(.*)\"\)\]', "[assembly: AssemblyVersion(""$versionUntilDash"")]" } `
+    | foreach { $_ -replace '\[assembly: AssemblyFileVersion\(\"(.*)\"\)\]', "[assembly: AssemblyFileVersion(""$versionUntilDash"")]" } `
+    | foreach { $_ -replace '\[assembly: AssemblyInformationalVersion\(\"(.*)\"\)\]', "[assembly: AssemblyInformationalVersion(""$longversion"")]" } `
+    | sc -Encoding UTF8 $file.FullName
+}
+
+foreach ($file in dir -Recurse $path | where { $_.Name -like "*.nuspec" }) {
+    (gc $file.FullName) `
+    | foreach { $_ -replace '<version>(.*)</version>', "<version>$version</version>" } `
+    | sc -Encoding UTF8 $file.FullName
+}
+
+#dir -Recurse $path | where { $_.Name -eq "AssemblyInfo.cs" }
